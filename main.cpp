5b6c0552a6b97ad9bd1b170c94de2f6e8f7c1e9a
Ozren Ćirković <15786747+skrapulon@users.noreply.github.com>
2019-03-11 18:06:16 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,9 @@
+// my first program in C++
+#include <iostream> // load header iostream.h
+using namespace std; // this allows all elements in the std namespace to be accessed in an unqualified manner (without the std:: prefix)
+					// blank line does nothing
+int main()			// initiation of the main function
+{					// { indicates the beginning of the main function 
+	std::cout << "Hello World!"; // std::cout = standard character output << = instert into "Hello World" content inserted into the standard output, ; end of a statement 
+	cout << "Like this."; //comment on the 3rd line
+}					// end of main function 
