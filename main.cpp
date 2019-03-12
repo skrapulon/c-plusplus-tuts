f2f476b1bc1fb44041b12ab780ea7915966bb402
Ozren Ćirković <15786747+skrapulon@users.noreply.github.com>
2019-03-12 11:16:45 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,23 +1,16 @@
-// operating with variables
+#include <iostream>
+using namespace std;
 
-#include <iostream> // use iostream.h header file, contains basic functions and variables
-using namespace std; // tnx to this we dont have to type std::cout (just cout)
-
-int main () // initialize main function
+int main()
 {
-  // declaring variables:
-  int a, b; // a and b are of int value 4bytes of memory, ranges from (-2147483648 to 2147483647 )
-  int result; // result = int value
+  int a=5;   // initial value:5
+  int b(3);  // initial value:3
+  int c{2};  // initial value:2
+  int result; // initial value undetermined
 
-  // process:
-  a = 5; // straighforward declaring a=5
-  b = 2; // straighforward declaring b=2
-  a = a + 1; // declaring a = 5(previous value)+1
-  result = a - b; // 6-2
+  a = a + b; // 8
+  result = a - c; // 8-2
+  cout << result; // print result 
 
-  // print out the result:
-  cout << result; 
-
-  // terminate the program:
   return 0;
 }
