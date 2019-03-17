96658f82fd6184bc80872b8cc1e5f71420496b71
Ozren Ćirković <15786747+skrapulon@users.noreply.github.com>
2019-03-17 20:17:00 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,16 +1,75 @@
+/*
+
+// constants
+
+// literals
+a=5;
+
+
+// integers (1776, 666, -666)
+75        //decimal
+0113      //octal
+0x4b      //hexadecimal
+
+75 //int
+75u //unsigned int
+751 //long
+75u1 //unsigned long
+751u // unsigned long
+
+//floating point numbers
+
+3.14159 //3.14159
+6.023e23 // 6.02 x 10^23
+1.6e-19// 1.6x10^-19
+3.0 // 3.0
+
+3.14159L //long double
+6.02e23f// float
+
+//other literals
+
+bool foo = true ;
+bool bar = false;
+int* p = nullptr;
+
+////
+
+*/
+/*
 #include <iostream>
 using namespace std;
 
-int main()
+const double pi = 3.14159;
+const char newline = '\n';
+
+int main ()
 {
-  int a=5;   // initial value:5
-  int b(3);  // initial value:3
-  int c{2};  // initial value:2
-  int result; // initial value undetermined
+  double r=5.0;  //radius
+  double circle;
 
-  a = a + b; // 8
-  result = a - c; // 8-2
-  cout << result; // print result 
+  circle = 2 * pi * r;
+  cout << circle;
+  cout << newline;
+}
 
-  return 0;
+*/
+
+// Preprocessor definition (#define)
+
+
+#include <iostream>
+using namespace std;
+
+#define PI 3.14159
+#define NEWLINE '\n'
+
+int main ()
+{
+  double r=5.0;
+  double circle;
+
+  circle = 2*PI*r;
+  cout << circle;
+  cout << NEWLINE;
 }
