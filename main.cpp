21b6b526c082fe791940e6f32da87ba0771eb2e3
Ozren Ćirković <15786747+skrapulon@users.noreply.github.com>
2019-03-18 14:14:21 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
-//compound assignment operators
+// conditional operator
 #include <iostream>
 using namespace std;
 
 int main ()
 {
-  int a, b=3;
-  a = b;
-  a+=2; // equivalent to a=a+2
-  cout << a;
+  int a,b,c;
+
+  a=(b=2, b+6);
+  
+  c= (a>b) ? a:b;
+
+  cout << c << '\n';
 }
