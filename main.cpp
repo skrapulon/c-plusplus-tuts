19c91fea7582e940ff0cce8b47510358ada6c309
Ozren Ćirković <15786747+skrapulon@users.noreply.github.com>
2019-03-20 15:20:14 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,14 +1,23 @@
-// i/o example
+// operating with variables
 
-#include <iostream>
-using namespace std;
+#include <iostream> // use iostream.h header file, contains basic functions and variables
+using namespace std; // tnx to this we dont have to type std::cout (just cout)
 
-int main ()
+int main () // initialize main function
 {
-  int i;
-  cout <<"Please enter an integer value: ";
-  cin >> i;
-  cout << "The value you entered is " << i;
-  cout << " and its double is " <<  i*2 <<".\n";
+  // declaring variables:
+  int a, b; // a and b are of int value 4bytes of memory, ranges from (-2147483648 to 2147483647 )
+  int result; // result = int value
+
+  // process:
+  a = 5; // straighforward declaring a=5
+  b = 2; // straighforward declaring b=2
+  a = a + 1; // declaring a = 5(previous value)+1
+  result = a - b; // 6-2
+
+  // print out the result:
+  cout << result; 
+
+  // terminate the program:
   return 0;
 }
